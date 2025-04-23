2e3cfb6755f269c6478e6a70577599fd68d19d29
abhijitsahoo99 <abhijitsahooas.01@gmail.com>
2025-04-23 11:47:59 +0530

    hero section responsive

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -11,10 +11,10 @@ export const Hero: React.FC = () => {
       <div className="flex mb-[-41px] w-full flex-col items-stretch max-md:max-w-full max-md:mb-2.5">
         <div className="w-full text-black text-center max-md:max-w-full">
           <div className="flex w-full flex-col items-stretch justify-center max-md:max-w-full">
-            <h1 className="font-bold text-4xl md:text-5xl lg:text-7xl max-md:max-w-full max-md:text-3xl">
+            <h1 className="font-bold text-4xl md:text-7xl max-md:max-w-full max-md:text-7xl">
               GET DONE MORE EVERY WEEK
             </h1>
-            <p className="font-normal text-lg md:text-2xl lg:text-4xl mt-10 md:mt-16 max-md:max-w-full">
+            <p className="font-normal text-2xl md:text-4xl mt-10 md:mt-16 max-md:max-w-full">
               Get weekly email check-ins, AI-powered ratings, and personalized
               feedback to help you stay accountable and improve consistently.
             </p>
